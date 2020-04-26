56f1ef729eae77af4be0b42af233db92cd37e916
Federico Del Gaudio <federico.delgaudio@hinto.com>
2020-04-26 19:40:20 +0200

    added projects section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,17 @@ import Navbar from '@common/Navbar';
 
 import Header from '@sections/Header';
 import About from '@sections/About';
-import Brands from '@sections/Brands';
 import Team from '@sections/Team';
 import Faq from '@sections/Faq';
 import Footer from '@sections/Footer';
+import Projects from '@sections/Projects'
 
 const IndexPage = () => (
   <Layout>
     <Navbar />
     <Header />
     <About />
-    <Brands />
+    <Projects/>
     <Team />
     <Faq />
     <Footer />
